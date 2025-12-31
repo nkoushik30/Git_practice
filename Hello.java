b43469753880b2fb04e445601323f7937e189202
nkoushik30 <koushiknelluri827@gmail.com>
2025-12-31 19:12:55 +0530

    Hello file added to Second_branch

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+ 
+    public static void main(String[] args) {
+    
+        int x = 10;
+    String message = "Hello, World!";
+    System.out.println(message);
+    System.out.println(x);
+    
+}
+}
