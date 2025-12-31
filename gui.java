03a07a4f9ad7559d21067c908b096a7856c85078
NELLURI_KOUSHIK <160224734+nkoushik30@users.noreply.github.com>
2025-12-31 21:31:00 +0530

    Delete gui.java

--- gui.java
+++
@@ -1,8 +0,0 @@
-public class gui {
-    public static void main(String[] args) {
-     
-    System.out.println("This is a GUI class.");
-       System.out.println("Hello, World!");
-       System.out.println("Welcome to the GUI application.");
-    }
-}
